b966f4c05173002bd602485efd14850796df07f7
hhander <100754103+hhander@users.noreply.github.com>
2022-04-10 19:48:00 +0800

    Add files via upload

---
+++ c++ 4_10/c++ 4_10/test.cpp
@@ -0,0 +1,126 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+#include<string>
+//int main()
+//{
+//	/*printf("hello");*/
+//	cout << "hello";
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	cout << "a = " << a<<endl;
+//	return 0;
+//}
+
+#define MAX 100
+//enum day
+//{
+//	monday=1,
+//	tuesday
+//};
+//
+//int main()
+//{
+//	/*int a = MAX;
+//	printf("%d", a);*/
+//
+//int a = 12;
+//printf("一年有%d个月", a);
+//	//a = 11;
+//int 
+//
+//}
+
+
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int sum = num1 + num2;
+//	cout << sum<<endl;//endl换行符 "\n"
+//	printf("endl");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	while (1)
+//	{
+//		if (i == 5)
+//		{
+//			continue;
+//		}
+//		i++;
+//		printf("%d", i);
+//	}
+//}
+
+//int main()
+//{
+//	//int a = 0;
+//	//cin >> a;
+//	//cout << a;
+//	float a = 3.145159f;
+//	cout << a<<endl;
+//	printf("%.5f", a);
+//}
+
+//int main()
+//{
+//	char a = 'd';
+//	//printf("%d", a);*/
+//	//a = 'b';
+//	cout << 'a'-32 << endl;
+//	printf("%c\n", 'B' + 32);
+//	cout << sizeof(a) << "个字节";
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	string arr1 = "hello";
+//	cout << arr1<<endl;
+//	char arr2[] = "hello";
+//	printf("%s\n", arr2);
+//	cout << arr2 << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	string arr = "abcdef";
+//	char ch[] = "abc0";
+//	/*cin >> arr;*/
+//	/*cout << arr<< endl;
+//	cout << ch[2];*/
+//	//cin >> ch;
+//	//cout << ch;
+//
+//	bool flag = false;
+//	cout << flag;
+//	cout << "请给bool flag赋值" << endl;
+//	cin >> flag;
+//	cout << flag<<endl;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	float a = 10;
+//	float b = 20;
+//	float c = a / b;
+//	cout << c;
+//	return 0;
+//}
